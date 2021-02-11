0a071e8a328c1ec293bc54a96304c107cd2dc176
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-02-11 10:17:13 +0000

    importing types

--- src/components/DetailComponent.tsx
+++ src/components/DetailComponent.tsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import {RouteComponentProps} from 'react-router-dom'
 //this is essentially a typescript interface coming from react-router-dom
-
+import {ICustomInterface} from "../types/interfaces"
 
 // interface CustomInterface{
 //     title: string
 // }
+//this is the same as the function that is being imported from types folder
 
 // type MyMixedCustomInterface = RouteComponentProps & CustomInterface
-
-interface MyMixedCustomInterface extends RouteComponentProps {
-    title: string;
-}
-
+// interface MyMixedCustomInterface extends RouteComponentProps {
+//     title: string;
+// }
 // this extended interface is the same as both above
+
+
+type MyMixedCustomInterface = RouteComponentProps & ICustomInterface //using the imported function
 
 //type & interface can be used for the same purposes, but interfaces are: 
 //1) only for objects
